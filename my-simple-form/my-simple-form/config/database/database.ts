983cddc60ce84321c77830ec0ea7603280fabed4
APPLE <apple@Apple.local>
2023-12-21 12:41:07 +0100

    initial commit

---
+++ my-simple-form/my-simple-form/config/database/database.ts
@@ -0,0 +1,16 @@
+mysql: {
+    client: 'mysql2',
+    connection: {
+      host: Env.get('MYSQL_HOST'),
+      port: Env.get('MYSQL_PORT'),
+      user: Env.get('MYSQL_USER'),
+      password: Env.get('MYSQL_PASSWORD', ''),
+      database: Env.get('MYSQL_DB_NAME'),
+    },
+    migrations: {
+      naturalSort: true,
+    },
+    healthCheck: false,
+    debug: false,
+  }
+  
